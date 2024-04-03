2b014a9cdefd5a60f00f12b4563ae1b0638d450f
naorex <nao23r@gmail.com>
2024-04-03 21:02:28 +0900

    today i learned

---
+++ Ch.4-1 if文/00_if文.js
@@ -0,0 +1,11 @@
+// 比較演算子
+let a = 3;
+let b = 2;
+if (a > b) {
+    console.log("Correct");
+}
+// 論理型の変数
+let c = true;
+if (c) {
+    console.log("Correct2");
+}
---
+++ Ch.4-1 if文/01_if文2.js
@@ -0,0 +1,20 @@
+// 分岐の中に分岐を入れる
+let color = "YELLOW";
+if (color == "RED") {
+    console.log(color);
+}
+else {
+    if (color == "GREEN") {
+        console.log(color);
+    }
+    else {
+        console.log("ERROR!!");
+    }
+}
+// 三項演算子(if else 文の短縮形)
+let a = 7;
+let b = "";
+// aが7だったらbにラッキーを代入、そうでなければアンラッキーを代入
+a === 7 ? (b = "ラッキー") : (b = "アンラッキー");
+// 例：うるう年の判定処理
+let isLeapYear = 2024 % 400 === 0 ? true : 2024 % 4 === 0 && 2024 % 100 !== 0;
