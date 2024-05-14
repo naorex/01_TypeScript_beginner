23fd8a875823f00f1d7563f331ce398d96ba638f
naorex <nao23r@gmail.com>
2024-05-14 21:13:55 +0900

    today i learned

--- Ch.6-4 引数の分割代入/00_devided_query.js
+++ Ch.6-4 引数の分割代入/00_devided_query.js
@@ -9,3 +9,18 @@ function addition2([a, , b]) {
     console.log(a + b); // a=1, b=100
 }
 addition2([1, 10, 100]); // 1+100=101。10は無視
+// 分割代入のデフォルト引数
+function addition3([a = 0, b = 0, c = 0]) {
+    console.log(a + b + c);
+}
+console.log(addition3([1, 5])); // 6 (a=1,b=5,C=0)
+// 注意：すべてがゼロの場合でも、addition3([])のように空の配列を代入する必要あり
+// 掛け算関数
+function multiple([a = 0, b = 1] = [0, 0]) {
+    console.log(a * b);
+}
+// 以下の振る舞いをする。ややこしいので注意
+console.log(multiple()); // 0 (a=0, b=0)
+console.log(multiple([])); // 0 (a=0, b=1)
+console.log(multiple([2])); // 2 (a=2, b=1)
+console.log(multiple([2, 3])); // 6 (a=2, b=3)
