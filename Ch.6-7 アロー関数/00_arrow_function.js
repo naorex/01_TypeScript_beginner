aa405c9c9292f3bff4f7e623f8344fefd6f3c542
naorex <nao23r@gmail.com>
2024-05-21 22:15:43 +0900

    today i learned

---
+++ Ch.6-7 アロー関数/00_arrow_function.js
@@ -0,0 +1,22 @@
+// 従来の関数定義
+function hello(message) {
+    console.log(message);
+}
+// 従来の関数式
+const hello2 = function (message) {
+    console.log(message);
+};
+// アロー関数
+// function の記述を省略し、アロー演算子 => で記述。
+const hello3 = (message) => console.log(message);
+// アロー関数の型注釈
+// 従来の型注釈
+function mixer(fruits) {
+    return fruits + "ジュース";
+}
+console.log(mixer("りんご")); // りんごジュース
+// 関数式
+const mixer2 = (fruits) => {
+    return fruits + "ジュース";
+};
+console.log(mixer2("メロン")); // メロンジュース
